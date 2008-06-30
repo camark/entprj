df4edd69e9c99586b16d679196498b679c1b03c0
gm8pleasure <gm8pleasure@312af124-c837-0410-82d7-1bab21ac9627>
2008-06-30 07:03:48 +0000

    Add Blog Authorized Method

--- modules/helloblog.py
+++ modules/helloblog.py
@@ -2,10 +2,31 @@ import wsgiref.handlers
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
+from google.appengine.api import users
 
 class HelloBlog(webapp.RequestHandler):
-    def check_login(self):
-        pass
+
+    def initialize(self,request,response):
+        webapp.RequestHandler.initialize(self,request,response)
+
+        self.user = users.get_current_user()
+        self.is_login = (self.user != None)
+        self.is_admin = users.is_current_user_admin()
+        
+    def check_login(self, redirect_url='/'):
+        if self.is_login:
+            return True
+        else:
+            self.redirect(redirect_url)
+            return False
+
+    def check_admin(self, redirect_url='/'):
+        if self.is_admin:
+            return True
+        else:
+            self.redirect(redirect_url)
+            return False
+            
 
     def param(self,name,**kw):
         return self.request.get(name,**kw)
